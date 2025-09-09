6b270a2d5d23a3971c79e2d38c817d2df5b12191
MD ZAHED IQBAL <tajiniiuc222@gmail.com>
2025-09-09 12:14:54 +0600

    first commit

---
+++ lib/ui/utils/asset_paths.dart
@@ -0,0 +1,5 @@
+class AssetPaths {
+  static const String _imagePath = "assets/images";
+  static const String backgroundSvg = "$_imagePath/background.svg";
+  static const String logodSvg = "$_imagePath/logo.svg";
+}
